b18c898a088ca9a224d4b65ef2f6e8dd05505434
Yashvi Kothari <43522677+yashvikothari@users.noreply.github.com>
2024-01-12 17:01:39 +0000

    added nodejs and rebuild devcontainer

---
+++ 01_basejs/03_conversionOperation.js
@@ -0,0 +1,3 @@
+let score = 33
+/* example- score Engineering Graphics n Design Exam
+now try to know what is type of score ? bcoz values might be stored in string and from front-end value send to backend through a form */
