7aaa0dfe5d46b223e850a4bc54f77a7787140932
Yashvi Kothari <43522677+yashvikothari@users.noreply.github.com>
2024-01-20 10:47:47 +0000

    privat plug play and comment

---
+++ 01_basejs/03_01playingconversion.js
@@ -0,0 +1,17 @@
+let score4 = 33abc
+console.log(score4);
+console.log(typeof score4);
+console.log(typeof (score4));
+console.log(score4);
+let valueofsc4 = Number(score4)
+console.log(valueofsc4);
+
+/*
+INPUT : 33
+OUTPUT :
+33
+number
+number
+33
+33
+*/
